acd804129b640c00d2a54d92f7d610468f5b0dba
siripat <siripat_ji@onwr.go.th>
2024-04-12 17:41:54 +0700

    edit V.1

--- app/Http/Controllers/FindController.php
+++ app/Http/Controllers/FindController.php
@@ -24,57 +24,49 @@ class FindController extends Controller
     }
 
     public function getFindParking(Request $request)
-    {
-        $cardId = $request->input('card_id');
-        $sessions = ParkingSession::where('card_id', $cardId)->get();
-        $parkingSlots = [];
+{
+    $cardId = $request->input('card_id');
+    $sessions = ParkingSession::where('card_id', $cardId)
+                             ->orderBy('id', 'desc')
+                             ->get();
 
-        foreach ($sessions as $session) {
-            $parkingSlot = ParkingSlot::where('id', $session->parking_slot_id)
-                ->where('is_available', 1)
-                ->first();
+    $parkingSlots = [];
+    foreach ($sessions as $session) {
+        $parkingSlot = ParkingSlot::where('id', $session->parking_slot_id)
+                                 ->where('is_available', 1)
+                                 ->first();
+        if ($parkingSlot) {
+            $currentSlot = [
+                'slot_number' => $parkingSlot->slot_number,
+                'slot_code' => $parkingSlot->slot_code,
+            ];
+            $neighborSlots = ParkingSlot::whereIn('slot_number', [
+                $currentSlot['slot_number'] - 1,
+                $currentSlot['slot_number'],
+                $currentSlot['slot_number'] + 1,
+            ])
+            ->whereIn('slot_code', [
+                $currentSlot['slot_code'],
+                chr(ord($currentSlot['slot_code']) - 1),
+                chr(ord($currentSlot['slot_code']) + 1),
+            ])
+            ->where('id', '!=', $parkingSlot->id)
+            ->get(['slot_number', 'slot_code'])
+            ->toArray();
+            $neighborSlots = array_unique($neighborSlots, SORT_REGULAR);
+            $neighborSlots = array_filter($neighborSlots, function ($slot) use ($currentSlot) {
+                return $slot['slot_number'] !== $currentSlot['slot_number'] || $slot['slot_code'] !== $currentSlot['slot_code'];
+            });
+            $parkingSlots[] = [
+                'current_slot' => $currentSlot['slot_number'] . $currentSlot['slot_code'],
+                'neighbor_slots' => array_map(function($slot) {
+                    return $slot['slot_number'] . $slot['slot_code'];
+                }, $neighborSlots),
+            ];
 
-            if ($parkingSlot) {
-                $currentSlot = [
-                    'slot_number' => $parkingSlot->slot_number,
-                    'slot_code' => $parkingSlot->slot_code,
-                ];
-
-                $neighborSlots = ParkingSlot::whereIn('slot_number', [
-                    $currentSlot['slot_number'] - 1,
-                    $currentSlot['slot_number'],
-                    $currentSlot['slot_number'] + 1,
-                ])
-                    ->whereIn('slot_code', [
-                        $currentSlot['slot_code'],
-                        chr(ord($currentSlot['slot_code']) - 1),
-                        chr(ord($currentSlot['slot_code']) + 1),
-                    ])
-                    ->where('id', '!=', $parkingSlot->id)
-                    ->get(['slot_number', 'slot_code'])
-                    ->toArray();
-
-                $neighborSlots = array_unique($neighborSlots, SORT_REGULAR);
-                $neighborSlots = array_filter($neighborSlots, function ($slot) use ($currentSlot) {
-                    return $slot['slot_number'] !== $currentSlot['slot_number'] ||
-                        $slot['slot_code'] !== $currentSlot['slot_code'];
-                });
-
-                $parkingSlots[] = [
-                    'current_slot' => $currentSlot,
-                    'neighbor_slots' => array_values($neighborSlots),
-                ];
-
-                /*                 $currentSlot = $parkingSlots['current_slot'];
-                $neighborSlots = array_filter($parkingSlots['neighbor_slots'], function ($slot) use ($currentSlot) {
-                    return $slot['slot_number'] !== $currentSlot['slot_number'] || $slot['slot_code'] !== $currentSlot['slot_code'];
-                }); */
-            } else {
-                return response()->json([
-                    'error' => 'No available location found for the specified card',
-                ], 404);
-            }
         }
-        return response()->json($parkingSlots);
     }
+
+    return response()->json($parkingSlots);
+}
 }
